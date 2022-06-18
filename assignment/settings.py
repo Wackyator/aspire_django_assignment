0bd1748f09a64707b66ce5b18f629b3b8acb5dca
Wackyator <pratapsinghtarun9@gmail.com>
2022-06-18 14:35:20 +0530

    add db config

--- assignment/settings.py
+++ assignment/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f!m_qh!#&qzfvv(2k!e#31!6s&=crl_$y*xsev%mhu*+*7#19q'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-f!m_qh!#&qzfvv(2k!e#31!6s&=crl_$y*xsev%mhu*+*7#19q')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -79,9 +80,11 @@ WSGI_APPLICATION = 'assignment.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aspire_db',
-        'USER': 'toaster',
-        'PASSWORD': '',
+        'NAME': 'sql6500584',
+        'USER': 'sql6500584',
+        'PASSWORD': 'ZwDhE27dJ9',
+        'HOST': 'sql6.freemysqlhosting.net',
+        'PORT':'3306'
     }
 }
 
